dea47652ca04bda16c6f9a671c9a6dd502c82681
stovepipe <zach.mehlenbacher@gmail.com>
2022-05-06 02:44:00 -0400

    Test connection grabbing inputs, creating objects, and pushing to database

--- Test Connections/app.py
+++ Test Connections/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 #from flask_pymongo import PyMongo
 import pymongo
-from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -11,7 +10,9 @@ app = Flask(__name__)
 # mongo = PyMongo(app)
 
 #client = MongoClient("mongodb+srv://team3:vegas@cluster0.sqdyt.mongodb.net/test?retryWrites=true&w=majority")
-client = pymongo.MongoClient("mongodb://team3:vegas@cluster0-shard-00-00.vttn9.mongodb.net:27017,cluster0-shard-00-01.vttn9.mongodb.net:27017,cluster0-shard-00-02.vttn9.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-bu6vn9-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://team3:vegas@cluster0-shard-00-00.vttn9.mongodb.net:27017,cluster0-shard-00-01.vttn9.mongodb.net:27017,cluster0-shard-00-02.vttn9.mongodb.net:27017/NBA_odds?ssl=true&replicaSet=atlas-bu6vn9-shard-0&authSource=admin&retryWrites=true&w=majority")
+# the "Test" below is what created a new database named "Test"
+db = client.NBA_odds
 
 @app.route("/")
 def index():
@@ -23,7 +24,7 @@ def submit():
     print('Heres the Dictionary')
     print(request.args.to_dict())
     #print(input1)
-    db = client.get_database('NBA_odds')
+    # this "Test" below is what created the test collection within the "Test" database created above in ln 15
     collection = db.get_collection('Test')
     collection.insert_many([request.args.to_dict()])
 
@@ -32,7 +33,7 @@ def submit():
     #request.args.to_dict()
     #print(request.args.to_dict())
 
-    return 'asdvadbv'
+    return 'what goes here'
 
 if __name__ == "__main__":
    app.run()
--- Test Connections/static/js/app.js
+++ Test Connections/static/js/app.js
@@ -1,10 +1,12 @@
-
-var inputs = {};
-
 function grabInputs(){
-    var input_values = d3.select("input");
-    console.log(input_values)
-    d3.json("http://127.0.0.1:5000/submit?Test1=Hello&Test2=Guys").then(data => console.log(data));
+    home_away_input = document.getElementById('home_away').value
+    bet_ml_input = document.getElementById('bet_ml').value
+    value_ml_input = document.getElementById('value_ml').value
+    bet_spread_input = document.getElementById('bet_spread').value
+    value_spread_input = document.getElementById('value_spread').value
+    bet_ou_input = document.getElementById('bet_ou').value
+    value_ou_input = document.getElementById('value_ou').value
+    d3.json("http://127.0.0.1:5000/submit?home_away=" + home_away_input + "&bet_ml=" + bet_ml_input + "&value_ml=" + value_ml_input + "&bet_spread=" + bet_spread_input + "&value_spread=" + value_spread_input + "&bet_ou=" + bet_ou_input + "&value_ou=" + value_ou_input),then(data => console.log(data));
 
 }
 
@@ -13,7 +15,3 @@ d3.select("#submit_button").on("click",grabInputs);
 
 
 console.log('Hi')
-
-//var grabingInputs = d3.selectAll("input");
-//localURL = "http://127.0.0.1:5000/submit";
-//3.json(localURL).then(data => console.log(data));
